fde8a637518cfa449d51c89c4c38ca329579f104
shorinkiwata <feng13862095732@gmail.com>
2025-09-22 16:29:58 +0900

    1

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,13 @@ end
 set fish_greeting ""
 
 starship init fish | source
+zoxide init fish --cmd cd | source
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
